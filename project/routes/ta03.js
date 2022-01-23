419bb3e47e23cc05be1cba659d1199b8d78fbd69
Tanner Wilson <tw113@byui.edu>
2022-01-22 19:52:51 -0800

    week 3

--- project/routes/ta03.js
+++ project/routes/ta03.js
@@ -1,6 +1,11 @@
 //TA03 PLACEHOLDER
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+
+const fileContents = fs.readFileSync('./items.json');
+const products = JSON.parse(fileContents);
 
 router.get('/', (req, res, next) => {
   res.render('pages/ta03', {
@@ -8,6 +13,18 @@ router.get('/', (req, res, next) => {
     path: '/ta03', // For pug, EJS
     activeTA03: true, // For HBS
     contentCSS: true, // For HBS
+    products: products
+  });
+});
+
+router.post('/', (req, res, next) => {
+  const search = req.body.search;
+  const results = products.filter(product => product.tags.includes(search));
+  console.log(results);
+  res.render('pages/ta03', {
+    title: 'Team Activity 03',
+    path: '/ta03',
+    products: results
   });
 });
 
